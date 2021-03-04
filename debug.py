1741838251af2f4ef1c5f697178eb0dce69fb957
rifkynovrian <78537684+rifkynovrian@users.noreply.github.com>
2021-03-04 23:45:18 +0700

    Add files via upload

---
+++ debug.py
@@ -0,0 +1,8 @@
+# from send_email import extract_contacts
+from send_email import extract_summary
+import fire
+
+if __name__ == '__main__':
+  # Export to Fire
+    #   fire.Fire(extract_contacts)
+    fire.Fire(extract_summary)
